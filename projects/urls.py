db8badd8727e1d10ac0ddade18e21ac1c9cb9d85
Arthur Nangai <arthur.nangai@gmail.com>
2023-07-18 19:42:57 +0300

    Remove the index view func

--- projects/urls.py
+++ projects/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from projects.views import index, project_list, project_detail
+from projects.views import project_list, project_detail
+
 
 app_name = 'projects'
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('all', project_list, name='all'),
+    path('', project_list, name='home'),
     path('<int:pk>', project_detail, name='detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,10 +3,6 @@ from django.http import HttpResponse, HttpRequest
 from projects.models import Project
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world")
-
-
 def project_list(request: HttpRequest) -> HttpResponse:
     projects = Project.objects.all()
     return render(request, 'projects/index.html', {
