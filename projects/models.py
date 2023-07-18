a7f859b9a19336bd1ab7093078000cee49a300f7
Arthur Nangai <arthur.nangai@gmail.com>
2023-07-18 17:14:37 +0300

    Update index template

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="projects/static/projects/img")
+    image = models.CharField(max_length=100)
  
     def __str__(self) -> str:
         return self.title
